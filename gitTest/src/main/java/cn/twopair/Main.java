3488b7b73c5658752b83709e5fe94904689557e1
ljjtpcn <ljjtpcn@163.com>
2022-06-30 15:37:34 +0800

    新行

--- gitTest/src/main/java/cn/twopair/Main.java
+++ gitTest/src/main/java/cn/twopair/Main.java
@@ -7,5 +7,7 @@ package cn.twopair;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("new line");
+
     }
 }
