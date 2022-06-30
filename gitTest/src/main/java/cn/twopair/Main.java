ed6ef9812e30360fcf83d8b5340b487385381afd
ljjtpcn <ljjtpcn@163.com>
2022-06-30 15:38:04 +0800

    新二行

--- gitTest/src/main/java/cn/twopair/Main.java
+++ gitTest/src/main/java/cn/twopair/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("new line");
-
+        System.out.println("新二行");
     }
 }
